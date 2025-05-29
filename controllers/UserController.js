b90d4ce5db3335892f56a43f9c388870276c6b27
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-05-29 18:17:22 +0200

    Agregar funcionalidad de inicio de sesión y autenticación de usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { jwt_secret } = require("../config/keys");
 
 const UserController = {
   async createUser(req, res) {
@@ -9,6 +11,22 @@ const UserController = {
       console.error(error);
     }
   },
+
+  async loginUser(req,res) {
+     try {
+        const user = await User.findOne({
+            email: req.body.email,
+        })
+      const token = jwt.sign({ _id: user._id }, jwt_secret);
+        if (user.tokens.length > 4) user.tokens.shift();
+        user.tokens.push(token);
+        await user.save();
+        res.send({ message: 'Bienvenid@ ' + user.username, token });
+    } catch (error) {
+        console.error(error);
+    }
+},
+
 };
 
 module.exports = UserController;
