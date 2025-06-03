84c822fb14981c888aa1b38fa8d5e8121647ed3e
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2025-06-03 12:47:38 +0200

    Agregar pruebas para el inicio y cierre de sesión de usuarios en user.test.js

--- tests/user.test.js
+++ tests/user.test.js
@@ -32,3 +32,63 @@ describe("testing/users", () => {
     expect(resUser.body.user.updatedAt).toBeDefined();
   });
 });
+
+
+describe("testing/login", () => {
+  const user = {
+    username: "TestUser",
+    email: "test123@gmail.com",
+    password: "test123",
+    confirmPassword: "test123",
+  };
+
+  beforeEach(async () => {
+    await User.deleteMany({});
+    await request(app).post("/user/create").send(user);
+  });
+
+  test("Login a user", async () => {
+    const res = await request(app)
+      .post("/user/login")
+      .send({ email: user.email, password: user.password })
+      .expect(200);
+
+    expect(res.body).toHaveProperty("token");
+    expect(res.body.message).toContain("Bienvenid@");
+    console.log("Token recibido:", res.body.token);
+  });
+});
+
+describe("testing/logout", () => {
+  const user = {
+    username: "TestUser",
+    email: "test123@gmail.com",
+    password: "test123",
+    confirmPassword: "test123",
+  };
+
+  beforeEach(async () => {
+    await User.deleteMany({});
+    await request(app).post("/user/create").send(user);
+  });
+
+  test("Logout a user", async () => {
+     // 1. Login para obtener el token
+    const loginRes = await request(app)
+      .post("/user/login")
+      .send({ email: user.email, password: user.password })
+      .expect(200);
+
+       const token = loginRes.body.token;
+    expect(token).toBeDefined();
+
+    // 2. Hacer logout usando el token
+    const logoutRes = await request(app)
+      .delete("/user/logout")
+      .set("Authorization", `${token}`)
+      .expect(200);
+
+    expect(logoutRes.body.message).toMatch(/Desconectado con éxito|success/i);
+    console.log("Logout:", logoutRes.body.message);
+  });
+});
